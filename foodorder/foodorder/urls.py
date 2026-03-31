625445d17cd977be92f62f80a2dbc3843e4c23d5
Harsh Gupta <harshg889691@email.com>
2026-03-31 20:38:40 +0530

    added cloudnery functionality

--- foodorder/foodorder/urls.py
+++ foodorder/foodorder/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
